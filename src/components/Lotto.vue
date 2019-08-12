c0fbeb5d9136b490cf6a6f9fe87107706ad336a5
Andy Terry <andyterrydesign@gmail.com>
2019-08-12 17:20:58 +0100

    updated lotto, balls now spin

--- src/components/Lotto.vue
+++ src/components/Lotto.vue
@@ -3,14 +3,35 @@
     <h1>LOTTO RESULTS</h1>
     <p>Monday 12th August 2019</p>
 
-    <div class="number">
+    <section class="stage">
+      <figure class="ball">
+        <span class="number" data-number="8">&nbsp;</span>
+      </figure>
+    </section>
+    <section class="stage">
+      <figure class="ball">
+        <span class="number" data-number="2">&nbsp;</span>
+      </figure>
+    </section>
+    <section class="stage">
+      <figure class="ball">
+        <span class="number" data-number="4">&nbsp;</span>
+      </figure>
+    </section>
+    <section class="stage">
+      <figure class="ball">
+        <span class="number" data-number="5">&nbsp;</span>
+      </figure>
+    </section>
+
+    <!-- <div class="number">
       <ul>
         <li>12</li>
         <li>15</li>
         <li>16</li>
         <li>44</li>
       </ul>
-    </div>
+    </div>-->
     <p>This weeks lottery Winners</p>
   </div>
 </template>
