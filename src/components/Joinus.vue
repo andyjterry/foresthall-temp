27b8df9ff4f565270a8396eba5f41cba972261ae
Andy Terry <andyterrydesign@gmail.com>
2019-08-14 17:04:57 +0100

    created build of app, changed asset paths to external

---
+++ src/components/Joinus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="joinus">
+    <h1>We are looking for volunteers to coach our teams</h1>
+    <p>If you are interested or would like more information please contact us at info@foresthalljuniors.co.uk</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/TeamVacancy.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="teamvacancy">
+    <div class="vacancy-header">
+      <h1>Looking To Join a Team</h1>
+      <p>We have spaces in the following teams.</p>
+    </div>
+    <ul v-for="(d,index) in dataArray" :key="index">
+      <!-- <a v-bind:href="`tel: + {{d.phone}}`"> -->
+      <li>u{{ d.age }} {{ d.team }}'s : {{ d.fname}} {{ d.phone }}</li>
+      <!-- </a> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dataArray: [
+        {
+          fname: "Steve",
+          sname: "Jamieson",
+          age: 12,
+          team: "Phantoms",
+          phone: "07305429345"
+        },
+        {
+          fname: "Andy",
+          sname: "Terry",
+          age: 12,
+          team: "Lions",
+          phone: "07447460461"
+        },
+        {
+          fname: "Nicola",
+          sname: "Chapman",
+          age: 8,
+          team: "",
+          phone: "07590027789"
+          // },
+          // {
+          //   fname: "Andy",
+          //   sname: "Terry",
+          //   age: 12,
+          //   team: "Lions",
+          //   phone: "07447460461"
+          // },
+          // {
+          //   fname: "Steve",
+          //   sname: "Jamieson",
+          //   age: 12,
+          //   team: "Phantoms",
+          //   phone: "07305429345"
+          // },
+          // {
+          //   fname: "Andy",
+          //   sname: "Terry",
+          //   age: 12,
+          //   team: "Lions",
+          //   phone: "07447460461"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
